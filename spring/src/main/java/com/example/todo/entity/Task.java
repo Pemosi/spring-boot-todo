8e0339e38cd9b7f89ab96d08234095516213d63f
Pemoshi <reo190328@gmail.com>
2024-12-14 11:32:42 +0900

    add: 基底EntityのBaseEntityを作成

--- spring/src/main/java/com/example/todo/entity/Task.java
+++ spring/src/main/java/com/example/todo/entity/Task.java
@@ -6,27 +6,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
-@Table(name = "projects")
+@Table(name = "tasks")
 @EqualsAndHashCode(callSuper = false)
-public class Project extends BaseEntity{
+public class Task extends BaseEntity{
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
-  @Column(nullable = false, length = 255)
+  @ManyToOne
+  @JoinColumn(name = "project_id")
+  private Project project;
+
+  @Column(length = 255, nullable = false)
   private String name;
 
-  @Column(columnDefinition = "TEXT")
-  private String summary;
+  @Column(nullable = false, columnDefinition = "SMALLINT")
+  private Integer priority;
 
-  @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+  @Column(columnDefinition = "TEXT")
+  private String memo;
+
+  @Column(name = "deadline_at")
+  private LocalDateTime deadlineAt;
+
+  @Column(name = "completed_at")
+  private LocalDateTime completedAt;
 
 }
