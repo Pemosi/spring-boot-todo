306b65873482e8c5f170598be4935416f0981c31
Pemoshi <reo190328@gmail.com>
2025-01-15 12:39:45 +0900

    update: Service で データ取得処理を実装

--- spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
@@ -1,13 +1,25 @@
 package com.example.todo.service.tasks;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.request.tasks.TaskSearchRequest;
 import com.example.todo.dto.response.TaskBaseResponse;
+import com.example.todo.repository.TaskRepository;
+import com.example.todo.entity.Task;
 
 @Service
 public class TaskSearchService {
+
+  private final TaskRepository taskRepository;
+
+  public TaskSearchService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
   public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
-    
+   List<Task> tasks =  this.taskRepository.findAll();
+
+   return tasks.stream().map(task -> new TaskBaseResponse(task) ).collect(Collectors.toList());
   }
 }
