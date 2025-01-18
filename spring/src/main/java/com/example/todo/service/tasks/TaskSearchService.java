c4f577d12dce9d76b76b9a5d09436c25911e163b
Pemoshi <reo190328@gmail.com>
2025-01-18 10:40:49 +0900

    update: Specification を利用し、検索条件と並び順を制御できるように実装

--- spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
@@ -2,11 +2,14 @@ package com.example.todo.service.tasks;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.request.tasks.TaskSearchRequest;
 import com.example.todo.dto.response.TaskBaseResponse;
 import com.example.todo.repository.TaskRepository;
+import com.example.todo.repository.specification.TaskSpecification;
 import com.example.todo.entity.Task;
+import org.springframework.data.domain.Sort;
 
 @Service
 public class TaskSearchService {
@@ -18,7 +21,11 @@ public class TaskSearchService {
   }
 
   public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
-   List<Task> tasks =  this.taskRepository.findAll();
+    TaskSpecification taskSpec = new TaskSpecification();
+    Specification<Task> spec = Specification.where(taskSpec.projectIdEquals(request.getProjectId()));
+    Sort sort = Sort.by(Sort.Order.desc("completedAt"), Sort.Order.asc("deadlineAt"),
+        Sort.Order.desc("id"));
+    List<Task> tasks =  this.taskRepository.findAll(spec, sort);
 
    return tasks.stream().map(task -> new TaskBaseResponse(task) ).collect(Collectors.toList());
   }
